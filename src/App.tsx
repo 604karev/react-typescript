ab510993c4c6dca96fd63b60122decd1c772b801
604karev <604karev@gmail.com>
2022-03-15 13:52:40 +0300

    feat: add CustomInput component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
 import React from 'react';
+import { CustomInput } from './components/CustomInput';
 import { Headline } from './components/Headline';
 
 function App() {
   return (
     <div className="App">
-      <Headline>
-        <div>Hello world</div>
-      </Headline>
+      <div className="container-sm">
+        <Headline>
+          <div>Hello world</div>
+        </Headline>
+        <div>
+          <CustomInput defaultValue='Some value' />
+        </div>
+      </div>
     </div>
   );
 }
